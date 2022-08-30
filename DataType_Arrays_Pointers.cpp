f1004bbc34c4fec7679de3a66882a2421e6f2541
Anton <106614976+AnthonyRom12@users.noreply.github.com>
2022-08-31 00:08:05 +0300

    Create DataType_Arrays_Pointers.cpp

---
+++ DataType_Arrays_Pointers.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int ages[5], age;
+    for (int i = 0; i < 5; ++i) {
+        cin >> ages[i]; 
+    }
+    age = ages[0];
+
+    for (int i = 0; i < 5; ++i) {
+        if (ages[i] < age)
+        age = ages[i];
+    }
+    cout << 50 - (50.0 * age / 100);
+    return 0;
+}
